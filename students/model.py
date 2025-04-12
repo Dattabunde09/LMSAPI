fa465fb5c4d8ac4600afb8dc3c338cf281220cce
Dattabunde09 <bundedatta@gmail,com>
2025-04-12 09:02:06 +0530

    id changed to int

--- students/model.py
+++ students/model.py
@@ -4,7 +4,7 @@ from utils.database import Base
 class Student(Base):
     __tablename__ = 'students'
 
-    id = Column(String(64), primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     email=Column(String(100),nullable=False)
     phone=Column(String(100),nullable=False)
